736febea7db28988909d4f8f47c71fecb950ad92
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-08-25 01:31:15 +0200

    Adding quote informations

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -6,9 +6,12 @@
 // }
 export class Quote {
     showDescription: boolean;
-    constructor(public quote_id: number,public quote_name: string,public quote_description: string){
+    // constructor(public quote_id: number,public quote_name: string,public quote_description: string){
+      constructor(public quote_id: number,public quote_name: string,public quote_author: string, public completeDate: Date,public quote_submitter){
       this.showDescription=false;
     }
   }
 
 
+
+  
